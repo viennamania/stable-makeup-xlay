d5b60c074883286c399934d601e06c006e0625b6
seunghyonpark <creath.park@gmail.com>
2025-09-18 16:20:17 +0900

    Change account number input type from number to text for better flexibility

--- src/app/[lang]/admin/store/[storecode]/settings/page.tsx
+++ src/app/[lang]/admin/store/[storecode]/settings/page.tsx
@@ -3229,18 +3229,11 @@ export default function SettingsPage({ params }: any) {
                                         <option value="산업은행">산업은행</option>
                                     </select>
                                     <input
-                                        type="number"
+                                        type="text"
                                         className="bg-white text-zinc-500 rounded-lg p-2 text-sm w-full"
                                         placeholder="계좌번호"
                                         value={withdrawalAccountNumber}
                                         onChange={(e) => setWithdrawalAccountNumber(e.target.value)}
-                                        // hide up and down arrow
-                                        onKeyDown={(e) => {
-                                            if (e.key === 'ArrowUp' || e.key === 'ArrowDown') {
-                                                e.preventDefault();
-                                            }
-                                        }}
-                                        onWheel={(e) => e.currentTarget.blur()}
                                     />
                                     <input
                                         type="text"
@@ -3370,19 +3363,11 @@ export default function SettingsPage({ params }: any) {
 
 
                                     <input
-                                        type="number"
+                                        type="text"
                                         className="bg-white text-zinc-500 rounded-lg p-2 text-sm w-full"
                                         placeholder="계좌번호"
                                         value={accountNumber}
                                         onChange={(e) => setAccountNumber(e.target.value)}
-                                        // hide up and down arrow
-                                        onKeyDown={(e) => {
-                                            if (e.key === 'ArrowUp' || e.key === 'ArrowDown') {
-                                                e.preventDefault();
-                                            }
-                                        }}
-                                        onWheel={(e) => e.currentTarget.blur()}
-
                                     />
                                     <input
                                         type="text"
